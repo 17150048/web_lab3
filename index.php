ac2d4bf0b65bf4722c24d3b9e65337f25d59b17d
ozzmo <cyram99@mail.ru>
2021-02-25 21:02:53 +0500

    Шаблон поста

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Валиханов 3</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  
+  <div class="posts">
+    <div class="post">
+      <img src="assets/1.jpg" alt="" class="post__image">
+      <h2 class="post__head">Пример новости</h2>
+      <p class="post__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo doloribus pariatur architecto voluptatum! Natus, nulla.</p>
+    </div>
+  </div>
 </body>
 </html>
